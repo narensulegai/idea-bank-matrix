2aa324c4aa54b79ffc67353b6c7127b2bcbfb8ef
Naren Sulegai <86naren@gmail.com>
2019-06-18 21:14:42 +0530

    Updates

--- src/components/Styled/index.js
+++
@@ -1,9 +0,0 @@
-import Fab from "@material-ui/core/Fab/index";
-import Paper from '@material-ui/core/Paper';
-import {styled} from '@material-ui/styles';
-
-export const PaddedPaper = styled(Paper)({
-  padding: '15px',
-  marginTop: '10px'
-});
-
